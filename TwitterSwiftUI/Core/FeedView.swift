2681060fc73f6b2ce8f15d4a3beb3548ce3dbfa1
パクギョンソク <boardguy1024@gmail.com>
2023-09-21 23:02:03 +0900

    First commit

---
+++ TwitterSwiftUI/Core/FeedView.swift
@@ -0,0 +1,20 @@
+//
+//  FeedView.swift
+//  TwitterSwiftUI
+//
+//  Created by パクギョンソク on 2023/09/21.
+//
+
+import SwiftUI
+
+struct FeedView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FeedView_Previews: PreviewProvider {
+    static var previews: some View {
+        FeedView()
+    }
+}
