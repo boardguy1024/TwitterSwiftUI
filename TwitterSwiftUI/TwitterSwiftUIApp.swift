ebe1650c1c294ddbbbf5d561b4be5957e296c6d8
パクギョンソク <boardguy1024@gmail.com>
2023-09-21 22:48:09 +0900

    Initial Commit

---
+++ TwitterSwiftUI/TwitterSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterSwiftUIApp.swift
+//  TwitterSwiftUI
+//
+//  Created by パクギョンソク on 2023/09/21.
+//
+
+import SwiftUI
+
+@main
+struct TwitterSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
